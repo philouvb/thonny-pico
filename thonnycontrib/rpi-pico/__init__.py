fd92d6d43509fa6b124030643a821c3315636c07
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-09-25 19:00:18 +0300

    Add Pico backend

--- thonnycontrib/rpi-pico/__init__.py
+++ thonnycontrib/rpi-pico/__init__.py
@@ -1,3 +1,30 @@
+from thonny.plugins.micropython import (
+    BareMetalMicroPythonProxy,
+    add_micropython_backend,
+    BareMetalMicroPythonConfigPage,
+)
+
 
 class RaspberryPiPicoBackendProxy(BareMetalMicroPythonProxy):
+    @property
+    def consider_unknown_devices(self):
+        return False
+
+    @property
+    def known_usb_vids_pids(self):
+        return {(0xF055, 0x9802)}
+
+
+class RaspberryPiPicoBackendConfigPage(BareMetalMicroPythonConfigPage):
     pass
+
+
+def load_plugin():
+    add_micropython_backend(
+        "RPiPico",
+        RaspberryPiPicoBackendProxy,
+        "MicroPython (Raspberry Pi Pico)",
+        RaspberryPiPicoBackendConfigPage,
+        bare_metal=True,
+        sort_key="32",
+    )
